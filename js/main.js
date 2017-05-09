ef3e81c43da18aae5f65cae4a956e76e00fe50bc
Philg112 <phil.grujovski@gmail.com>
2017-05-09 21:53:44 +1000

    Inital Commit

---
+++ js/main.js
@@ -0,0 +1,79 @@
+// ------- START FUNCTIONS -------
+$(document).ready(function() {
+    getPeopleInSpace();
+    getIssLocation();
+    window.setInterval(function() {
+        getIssLocation();
+    }, 3000);
+    
+})
+
+// ----------- PPL IN SPACE -----------
+var NUM_OF_PPL_URL = "http://api.open-notify.org/astros.json"
+
+function getPeopleInSpace() {
+    $.ajax({
+        url: NUM_OF_PPL_URL,
+        method: "GET",
+        dataType: "JSONP"   
+    }).done(displayPeople);
+}
+
+function displayPeople(response) {
+    var numOfPeople = response.number;
+    $(".num-ppl").text(numOfPeople);
+    for (var i = 0; i < numOfPeople; i++) {
+        var $newPara = $("<p></p>")
+        $newPara.text(response.people[i].name);
+        $newPara.appendTo(".people-results");
+    }
+}
+
+// ------------ ISS -----------
+var ISS_URL = "http://api.open-notify.org/iss-now.json"
+
+function getIssLocation() {
+    $.ajax({
+        url: ISS_URL,
+        method: "GET",
+        dataType: "JSONP"
+    }).done(displayIssLocation);
+}
+
+function displayIssLocation(response) {
+    var longitude = parseFloat(response.iss_position.longitude);
+    var latitude = parseFloat(response.iss_position.latitude);
+    $(".iss-longitude").text(longitude);
+    $(".iss-latitude").text(latitude);
+    initMap(latitude, longitude);
+}
+
+// ------ GOOGLE MAP TRACKING -------
+var map;
+var poly;
+function initMap(lat, long) {    
+    var iss = {lat: lat, lng: long};
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: iss
+    });
+    marker = new google.maps.Marker({
+        title: "ISS",
+        position: iss,
+        map: map
+        // icon: "assets/ufo-icon.png"
+    });
+
+    poly = new google.maps.Map(document.getElementById('map-poly'), {
+        zoom: 3,
+        center: iss
+    });
+    var issPath = new google.maps.Polyline({
+        map: poly,
+        path: iss,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+}
